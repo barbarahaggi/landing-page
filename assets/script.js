5119403980b445769e3010ec3cccd90f105e2334
Barbara Haggi <barbarahaggi3@gmail.com>
2022-11-15 13:12:17 +0000

    landing page

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('menu');
+const navMenu = document.querySelector('click', () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+})
